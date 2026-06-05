00b73df4dbf4c09bbc3a3fdfd5d9a9efb41159e9
Supratim Dhara <supratimdhara0@gmail.com>
2026-06-05 12:07:39 +0530

    fix: force static generation for sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { getSortedPostsData } from '@/lib/blog'
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://supratimdev.qzz.io'
   
